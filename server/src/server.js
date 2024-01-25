9b53e5a691015d7ed6b1712b4e33fd6c273f8e22
lisiCAO <caolisi@hotmail.com>
2024-01-24 21:34:30 -0500

    initial project

---
+++ server/src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// define routes
+// app.use('/api', require('./routes/api'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
