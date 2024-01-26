7f8cf3fb47c550f2b0176c579790dc33ef4a3fb6
lisiCAO <caolisi@hotmail.com>
2024-01-26 14:07:53 -0500

    create auth page

---
+++ client/src/pages/AuthPage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const AuthPage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="container mt-5">
+      <div className="text-center">
+        <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+        <button 
+          className="btn btn-lg btn-primary btn-block"
+          onClick={() => navigate('/login-with-google')} // TODO: implement， path: {backend}/auth/google
+        >
+          Continue with Google
+        </button>
+        <button 
+          className="btn btn-lg btn-secondary btn-block"
+          onClick={() => navigate('/login')}
+        >
+          Login
+        </button>
+        <button 
+          className="btn btn-lg btn-success btn-block"
+          onClick={() => navigate('/register')}
+        >
+          Register
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AuthPage;
