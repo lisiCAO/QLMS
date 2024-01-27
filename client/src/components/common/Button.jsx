099ac41f2dace2ac6ed99e23f4de578a9136b4b1
lisiCAO <caolisi@hotmail.com>
2024-01-27 18:53:32 -0500

    delete unuse components

--- client/src/components/common/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Button = ({ type, className, text, onClick }) => (
-  <button type={type} className={className} onClick={onClick}>
-    {text}
-  </button>
-);
-
-export default Button;
