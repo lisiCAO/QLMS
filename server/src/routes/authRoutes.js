9b48f80ef900e4692e73e188667b58c0eedfe128
lisiCAO <caolisi@hotmail.com>
2024-01-28 16:04:01 -0500

    init authRoutes

---
+++ server/src/routes/authRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const passport = require('passport');
+const authController = require('../controllers/authController');
+const router = express.Router();
+
+// Google OAuth login route
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+// Google OAuth callback route
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), authController.googleAuthCallback, function (req, res) {
+    res.cookie("jwt", req.user.token, { httpOnly: true });
+    console.log("req.user.token", req.user.token);
+    res.redirect("/");
+});
+
+// Other OAuth login routes
+
+module.exports = router;
