9d92c1c134863228fec2d92949b9b0ba526e2749
lisiCAO <caolisi@hotmail.com>
2024-01-28 13:37:53 -0500

    format

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,20 +1,20 @@
 const jwt = require('jsonwebtoken');
-const db = require('../models/index'); 
+const db = require('../models/index');
 const OAuthToken = db.oauth_token;
 
-const User = db.user; 
+const User = db.user;
 
 const findOrCreateUser = (userData, callback) => {
-    User.findOrCreate({
-        where: { oauth_provider_user_id: userData.oauth_provider_user_id, oauth_provider: userData.oauth_provider },
-        defaults: userData
-      })
-      .then(([user, created]) => {
-        callback(null, user);
-      })
-      .catch(err => {
-        callback(err, null);
-      });
+  User.findOrCreate({
+    where: { oauth_provider_user_id: userData.oauth_provider_user_id, oauth_provider: userData.oauth_provider },
+    defaults: userData
+  })
+    .then(([user, created]) => {
+      callback(null, user);
+    })
+    .catch(err => {
+      callback(err, null);
+    });
 };
 
 exports.googleAuthCallback = (accessToken, refreshToken, profile, done) => {
@@ -24,34 +24,32 @@ exports.googleAuthCallback = (accessToken, refreshToken, profile, done) => {
     email: profile.emails[0].value,
     first_name: profile.name.givenName,
     last_name: profile.name.familyName,
-    username:  profile.emails[0].value,
-    role: "tenant" 
+    username: profile.emails[0].value,
+    role: "tenant"
   };
 
   findOrCreateUser(userData, (err, user) => {
     if (err) {
-        return done(err);
-      }
-
-saveOrUpdateOAuthToken(user.id, accessToken, refreshToken, profile.provider, profile.id);
-
-
+      return done(err);
+    }
+    saveOrUpdateOAuthToken(user.id, accessToken, refreshToken, profile.provider, profile.id);
     const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
     done(null, { user, token });
   });
+
 };
 
 const saveOrUpdateOAuthToken = (userId, accessToken, refreshToken, provider, oauth_provider_user_id) => {
-    OAuthToken.upsert({
-      user_id: userId,
-      access_token: accessToken,
-      refresh_token: refreshToken,
-      expires_in: new Date(Date.now() + 3600000), 
-      provider: provider,
-      oauth_provider_user_id: oauth_provider_user_id
-    })
+  OAuthToken.upsert({
+    user_id: userId,
+    access_token: accessToken,
+    refresh_token: refreshToken,
+    expires_in: new Date(Date.now() + 3600000),
+    provider: provider,
+    oauth_provider_user_id: oauth_provider_user_id
+  })
     .then(() => console.log('OAuth Token saved or updated'))
     .catch(err => console.error('Error saving OAuth Token:', err));
-  }
+}
 
 // Other Methods
