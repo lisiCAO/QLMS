6c1f14ab2292ef119d8a50b9b5e903cd504d862f
ShixinTang <shixin.tang@outlook.com>
2024-01-29 10:37:30 -0500

    set db.sequelize.sync({ force: false })
    to avoid manually run db:seeder:all each time restart the server container

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const db = require('./models');
+const db = require("./models");
 const cors = require("cors");
 const session = require("express-session");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const {sendSuccessResponse,sendErrorResponse} = require("./middleWares/responseHandler");
+const {
+    sendSuccessResponse,
+    sendErrorResponse,
+} = require("./middleWares/responseHandler");
 require("dotenv").config();
 
 const app = express();
@@ -20,13 +23,14 @@ app.use(sendSuccessResponse);
 app.use(sendErrorResponse);
 
 // Session init
-app.use(session({
-    secret: process.env.SESSION_SECRET, // 用于签名 session ID 的秘钥，建议使用随机字符串
-    resave: false, // 强制保存 session 即使它没有变化
-    saveUninitialized: true, // 强制将未初始化的 session 存储
-    cookie: { secure: false }, // 如果为 true，则只通过 HTTPS 发送 cookie
-    // 注意：在生产环境中，应设置 secure 为 true，并确保网站使用 HTTPS
-})
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET, // 用于签名 session ID 的秘钥，建议使用随机字符串
+        resave: false, // 强制保存 session 即使它没有变化
+        saveUninitialized: true, // 强制将未初始化的 session 存储
+        cookie: { secure: false }, // 如果为 true，则只通过 HTTPS 发送 cookie
+        // 注意：在生产环境中，应设置 secure 为 true，并确保网站使用 HTTPS
+    })
 );
 
 // Passport Init
@@ -39,29 +43,37 @@ passport.deserializeUser((obj, done) => done(null, obj));
 
 const authController = require("./controllers/authController");
 // GoogleStrategy
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:8000/auth/google/callback"
-},
-// Handle by authController.googleAuthCallback function
-authController.googleAuthCallback
-));
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL:
+                process.env.GOOGLE_CALLBACK_URL ||
+                "http://localhost:8000/auth/google/callback",
+        },
+        // Handle by authController.googleAuthCallback function
+        authController.googleAuthCallback
+    )
+);
 
 // Database sync
-db.sequelize.sync({ force: true }) // force: true will drop the table if it already exists, default value is false, Production environment should set it to false
+db.sequelize
+    .sync({ force: false }) // force: true will drop the table if it already exists, default value is false, Production environment should set it to false
     .then(() => {
-        console.log('Database synced');
+        console.log("Database synced");
 
         // Do not do  anything before database sync
-        const authRoutes = require('./routes/authRoutes');
-        const propertyRoutes = require("./routes/propertyRoutes.js"); 
+        const authRoutes = require("./routes/authRoutes");
+        const propertyRoutes = require("./routes/propertyRoutes.js");
         // Import other routes...
 
         // Use routes
-        app.get("/", (req, res) => {res.json(`Hello, this is the QLMS backend server on port ${PORT}!`);});
-        app.use('/auth', authRoutes);
-        app.use('/api/properties', propertyRoutes);
+        app.get("/", (req, res) => {
+            res.json(`Hello, this is the QLMS backend server on port ${PORT}!`);
+        });
+        app.use("/auth", authRoutes);
+        app.use("/api/properties", propertyRoutes);
         // Use other routes...
 
         // Listen to port
@@ -70,7 +82,7 @@ db.sequelize.sync({ force: true }) // force: true will drop the table if it alre
         });
     })
     .catch((error) => {
-        console.error('Error syncing database: ', error);
+        console.error("Error syncing database: ", error);
     });
 
 // Path: server/src/routes/authRoutes.js
