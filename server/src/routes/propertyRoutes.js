a2815b3d5fb7b4ef4081cc262180c023e850bca3
ShixinTang <shixin.tang@outlook.com>
2024-01-29 21:22:16 -0500

    update property table manipulation
    update propertyRoutes to routes->controller->service architecture

--- server/src/routes/propertyRoutes.js
+++ server/src/routes/propertyRoutes.js
@@ -21,60 +21,19 @@ router.post(
 // TODO: Adjust the following routes to use propertyController
 
 // get all properties
-router.get("/", async (req, res) => {
-    try {
-        const properties = await property.findAll();
-        res.sendSuccess(properties, "Properties retrieved successfully");
-    } catch (error) {
-        res.sendError("Failed to retrieve properties,error" + error, 500);
-    }
-});
+router.get("/", propertyController.getAllProperties);
 
 // get single property
-router.get("/:id", async (req, res) => {
-    try {
-        const prop = await property.findByPk(req.params.id);
-        if (prop) {
-            res.sendSuccess(prop, "Property retrieved successfully");
-        } else {
-            res.sendError("Property not found", 404);
-        }
-    } catch (error) {
-        res.sendError("Failed to retrieve property: " + error.message, 500);
-    }
-});
+router.get("/:id", propertyController.getSingleProperty);
 
 // update single property
-router.put("/:id", async (req, res) => {
-    try {
-        const updated = await property.update(req.body, {
-            where: { id: req.params.id },
-        });
-        if (updated) {
-            const updatedProperty = await property.findByPk(req.params.id);
-            res.sendSuccess(updatedProperty, "Property updated successfully");
-        } else {
-            res.sendError("Property not found", 404);
-        }
-    } catch (error) {
-        res.sendError("Failed to update property: " + error.message, 500);
-    }
-});
+router.put(
+    "/:id",
+    propertyController.propertyValidationRules,
+    propertyController.updateProperty
+);
 
 // delete single property
-router.delete("/:id", async (req, res) => {
-    try {
-        const deleted = await property.destroy({
-            where: { id: req.params.id },
-        });
-        if (deleted) {
-            res.sendSuccess(null, "Property deleted successfully");
-        } else {
-            res.sendError("Property not found", 404);
-        }
-    } catch (error) {
-        res.sendError("Failed to delete property: " + error.message, 500);
-    }
-});
+router.delete("/:id", propertyController.deleteProperty);
 
 module.exports = router;
