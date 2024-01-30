fd1cdab6c313f8434cfc9d0f7777061591f233a1
ShixinTang <shixin.tang@outlook.com>
2024-01-29 20:12:45 -0500

    update seeder files

--- server/src/seeders/20240126203942-user-seeder.js
+++ server/src/seeders/20240126203942-user-seeder.js
@@ -3,23 +3,24 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-
         await queryInterface.bulkInsert(
             "user",
             [
                 {
                     username: "Tom",
-                    password_hash: "password",
+                    password_hash: "password123!",
                     email: "Tom@qlms.com",
                     role: "tenant",
+                    is_verified: false,
                     created_at: new Date(),
                     updated_at: new Date(),
                 },
                 {
                     username: "Jerry",
-                    password_hash: "password",
+                    password_hash: "password123!",
                     email: "Jerry@qlms.com",
                     role: "landlord",
+                    is_verified: false,
                     created_at: new Date(),
                     updated_at: new Date(),
                 },
@@ -29,7 +30,6 @@ module.exports = {
     },
 
     async down(queryInterface, Sequelize) {
-
         await queryInterface.bulkDelete("user", null, {});
     },
 };
