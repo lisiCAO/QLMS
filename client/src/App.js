67f0a2319e9f72dcc38575b4fbc898dc0453b86b
lisiCAO <caolisi@hotmail.com>
2024-01-31 02:26:47 -0500

    adjust comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from './context/authContext'; // Import AuthProvider
 import AuthPage from "./pages/AuthPage";
 import CreateProperty from "./components/properties/CreateProperty";
 import ListOfProperties from "./components/properties/ListOfProperties";
-import ProtectedRoute from "./ProtectedRoute"; // 引入 ProtectedRoute 组件，确保路径正确
+import ProtectedRoute from "./ProtectedRoute"; // Import ProtectedRoute
 import "./App.css";
-import { AuthProvider } from './context/authContext'; // 引入 AuthProvider，确保路径正确
 
 function App() {
   return (
@@ -13,16 +13,15 @@ function App() {
         <Router>
           <Routes>
             <Route path="/" element={<AuthPage />} />
-            <Route path="/:view" element={<AuthPage />} /> {/* AuthPage 作为默认页面 */}
+            <Route path="/:view" element={<AuthPage />} /> {/* AuthPage as default */}
 
-            {/* 使用 ProtectedRoute 保护需要认证的路由 */}
+            {/* Protected Routes  */}
             <Route element={<ProtectedRoute />}>
               <Route path="/property/create" element={<CreateProperty />} />
               <Route path="/property/list" element={<ListOfProperties />} />
-              {/* 在此处添加更多需要保护的路由 */}
             </Route>
             
-            {/* 可以添加更多不需要保护的路由 */}
+            {/* no Protected Routes */}
           </Routes>
         </Router>
     </AuthProvider>
