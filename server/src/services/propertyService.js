e1efc4056cff41daf3c476994be23127540057f5
ShixinTang <shixin.tang@outlook.com>
2024-02-01 16:51:10 -0500

    fix get properties and get property API  don't include image table info

--- server/src/services/propertyService.js
+++ server/src/services/propertyService.js
@@ -1,5 +1,5 @@
-const { property, user } = require("../models"); // Import property model
-const { sequelize } = require("../models/index");
+const { property, user, image } = require("../models"); // Import property model
+const { sequelize, Sequelize } = require("../models/index");
 const imageService = require("./imageService");
 
 // Service to create a new property
@@ -28,11 +28,14 @@ exports.createProperty = async (propertyData, userId, transaction) => {
         throw new Error("A property with the same address already exists.");
     }
     // Create property
-    try{
-        const newProperty = await property.create({
-            ...propertyData, 
-            owner_user_id: userId,
-        }, { transaction });
+    try {
+        const newProperty = await property.create(
+            {
+                ...propertyData,
+                owner_user_id: userId,
+            },
+            { transaction }
+        );
 
         return newProperty;
     } catch (error) {
@@ -46,15 +49,23 @@ exports.createProperty = async (propertyData, userId, transaction) => {
 };
 
 exports.createPropertyWithImages = async (propertyData, imagesData, userId) => {
-    const transaction = await sequelize.transaction(); 
+    const transaction = await sequelize.transaction();
 
     try {
         // Step 1: Create the property
-        const newProperty = await this.createProperty(propertyData, userId, transaction);
+        const newProperty = await this.createProperty(
+            propertyData,
+            userId,
+            transaction
+        );
         console.log("newProperty:", newProperty);
         // Step 2: Process images with imageService
         // Ensure imageService.saveImages is adapted to accept a transaction
-        const processedImages = await imageService.saveImages(imagesData, newProperty.id, transaction);
+        const processedImages = await imageService.saveImages(
+            imagesData,
+            newProperty.id,
+            transaction
+        );
         console.log("processedImages:", processedImages);
         // If all operations are successful, commit the transaction
         await transaction.commit();
@@ -72,11 +83,42 @@ exports.createPropertyWithImages = async (propertyData, imagesData, userId) => {
 };
 
 exports.getAllProperties = async () => {
-    return await property.findAll();
+    const sql = `SELECT * FROM property;`; // get all properties
+
+    const propertiesResult = await sequelize.query(sql, {
+        type: Sequelize.QueryTypes.SELECT,
+    });
+    console.log("propertiesResult= ", propertiesResult);
+
+    //for each property, get all images
+    for (const property of propertiesResult) {
+        const imageSql = `SELECT property_id, image_url, description, uploaded_at, is_primary FROM image WHERE property_id = ${property.id};`;
+        const imagesResult = await sequelize.query(imageSql, {
+            type: Sequelize.QueryTypes.SELECT,
+        });
+        property.image_urls = imagesResult.map((image) => image.image_url);
+    }
+
+    return propertiesResult;
 };
 
 exports.getSingleProperty = async (propertyId) => {
-    return await property.findByPk(propertyId);
+    const sql = `SELECT * FROM property where id = ${propertyId};`; // get single property by id
+
+    let properyResult = await sequelize.query(sql, {
+        type: Sequelize.QueryTypes.SELECT,
+    });
+    console.log("propertyResult= ", properyResult);
+
+    //for each property, get all images
+    const imageSql = `SELECT property_id, image_url, description, uploaded_at, is_primary FROM image WHERE property_id = ${properyResult[0].id};`;
+    const imagesResult = await sequelize.query(imageSql, {
+        type: Sequelize.QueryTypes.SELECT,
+    });
+    properyResult[0].image_urls = imagesResult.map((image) => image.image_url);
+
+    console.log("propertyResult= ", properyResult);
+    return properyResult;
 };
 
 exports.updateProperty = async (propertyId, propertyData) => {
