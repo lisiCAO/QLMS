9a149fdf28e475eaa530eff8afc747a1200ae6b4
lisiCAO <caolisi@hotmail.com>
2024-02-01 23:26:44 -0500

    fix auth context login, logout, user, role

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { SearchProvider } from "./context/SearchContext";
-import { AuthProvider } from "./context/AuthContext";
+
 
 import AuthPage from "./pages/AuthPage";
 import ProtectedRoute from "./services/ProtectedRoute";
@@ -20,8 +19,6 @@ import LandlordTenantPage from "./pages/landlord/tenant/LandlordTenantPage";
 
 function App() {
   return (
-    <AuthProvider> {/* Global user state management */}
-      <SearchProvider> {/* Global search state management */}
         <Router>
           <Routes>
             {/* Auth routes */}
@@ -39,15 +36,13 @@ function App() {
               
             </Route>
             {/* Landlord routes */}
-            <Route path="/landlord/*" element={<LandlordLayout />}>
+            <Route path="/landlord/*" element={<ProtectedRoute><LandlordLayout /></ProtectedRoute>}>
               <Route path="properties/*" element={<PropertyPage />} />
               <Route path="leases/*" element={<LeasePage />} />
               <Route path="tenants/*" element={<LandlordTenantPage />} />
             </Route>
           </Routes>
         </Router>
-      </SearchProvider>
-    </AuthProvider>
   );
 }
 
