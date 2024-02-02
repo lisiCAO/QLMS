2f72b8c826032d6b1f59410784d57fadacc2b7bd
lisiCAO <caolisi@hotmail.com>
2024-02-01 21:12:09 -0500

    add container

--- client/src/pages/landlord/LandlordLayout.jsx
+++ client/src/pages/landlord/LandlordLayout.jsx
@@ -3,6 +3,7 @@ import Sidebar from "./../../components/layout/Sidebar";
 import Topbar from "./../../components/layout/Topbar";
 import { Outlet } from "react-router-dom";
 import "./Layout.scss";
+import { Container } from "react-bootstrap";
 
 const LandlordLayout = () => {
   return (
@@ -11,7 +12,9 @@ const LandlordLayout = () => {
       <div className="landlord-content d-flex flex-column flex-grow-1">
         <Topbar />
         <div className="dynamic-content flex-grow-1">
-          <Outlet /> {/* Landlord child route */}
+          <Container fluid className="p-4 h-100">
+            <Outlet /> {/* Landlord child route */}
+          </Container>
         </div>
       </div>
     </div>
