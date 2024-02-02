540f8f7ca29d99586dea40eabeaa8c12f031f138
lisiCAO <caolisi@hotmail.com>
2024-02-02 00:54:14 -0500

    update profile

--- client/src/components/account/UserProfile.jsx
+++ client/src/components/account/UserProfile.jsx
@@ -1,58 +1,64 @@
-import { Box, Container, Stack, Typography, Unstable_Grid2 as Grid } from '@mui/material';
-import  AccountProfile from './AccountProfile';
-import  AccountProfileDetails  from './AccountProfileDetail';
+import React, { useEffect } from "react";
+import {
+  Box,
+  Container,
+  Stack,
+  Typography,
+  Unstable_Grid2 as Grid,
+} from "@mui/material";
+import { useAuth } from "./../../context/AuthContext";
+import ApiService from "./../../services/ApiService";
+import AccountProfile from "./AccountProfile";
+import AccountProfileDetails from "./AccountProfileDetail";
+const UserProfile = () => {
+  const { user } = useAuth();
+  const [userData, setUserData] = React.useState(null);
 
-// Account page, presumably displaying user account information
-const Page = () => {
-    return (
-      <>
-        <title>
-          Account 
-        </title>
-  
-        <Box
-          component="main"
-          sx={{
-            flexGrow: 1,
-            py: 8
-          }}
-        >
-          <Container maxWidth="lg">
-            <Stack spacing={3}>
-              <div>
-                <Typography variant="h4">
-                  Account
-                </Typography>
-              </div>
-              <div>
-                <Grid
-                  container
-                  spacing={3}
-                >
-                  <Grid
-                    xs={12}
-                    md={6}
-                    lg={4}
-                  >
-                    {/* AccountProfile component, presumably displaying user profile information */}
-                    <AccountProfile />
-                  </Grid>
-                  <Grid
-                    xs={12}
-                    md={6}
-                    lg={8}
-                  >
-                    {/* AccountProfileDetails component, presumably displaying additional details about the user's account */}
-                    <AccountProfileDetails />
-                  </Grid>
-                </Grid>
-              </div>
-            </Stack>
-          </Container>
-        </Box>
-      </>
-    );
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const userData = await ApiService.fetchUserData(user.uid);
+        setUserData(userData);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    }
+    fetchUserData();
   }
-  
-  export default Page;
-  
+  , [user.userId]);
+
+  return (
+    <>
+      <title>Account</title>
+
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container maxWidth="lg">
+          <Stack spacing={3}>
+            <div>
+              <Typography variant="h4">Account</Typography>
+            </div>
+            <div>
+              <Grid container spacing={3}>
+                <Grid xs={12} md={6} lg={4}>
+                  {/* AccountProfile component, presumably displaying user profile information */}
+                  <AccountProfile user={userData} />
+                </Grid>
+                <Grid xs={12} md={6} lg={8}>
+                  {/* AccountProfileDetails component, presumably displaying additional details about the user's account */}
+                  <AccountProfileDetails user={userData}/>
+                </Grid>
+              </Grid>
+            </div>
+          </Stack>
+        </Container>
+      </Box>
+    </>
+  );
+};
+export default UserProfile;
