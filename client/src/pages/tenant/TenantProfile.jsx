bdb519a6ca3a5f2d5c44a878767ae07819248a1c
lisiCAO <caolisi@hotmail.com>
2024-02-03 08:13:43 -0500

    fix imput

--- client/src/pages/tenant/TenantProfile.jsx
+++ client/src/pages/tenant/TenantProfile.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import UserProfile from '../../components/account/UserProfile';
+import ApiService from '../../services/ApiService';
 
 
 const TenantProfile = () => {
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const data = await ApiService.fetchUserData('user.userId');
+                setUserData(data);
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+                setError('Failed to fetch user data'); 
+            } finally {
+                setLoading(false); 
+            }
+        };
+
+        fetchUserData();
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>; //  Set up a loading state
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>; // 
+    }
 
     return (
         <div>
