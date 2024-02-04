45a71a90a8167e6bd6cc0a5c2e1e7a3e11f11dca
CAO LISI <137085653+lisiCAO@users.noreply.github.com>
2024-02-03 22:11:38 -0500

    Feature/client clean code (#87)
    * fix bugs
    
    * formate
    
    * link property list to lease create
    
    * connect tenant list
    
    * connect tenant list to edit
    
    * pre fill tenant info
    
    * tenant create connect
    
    * format

--- client/src/pages/aboutUs/AboutUsNavbar.jsx
+++ client/src/pages/aboutUs/AboutUsNavbar.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 const AboutUsNavbar = () => (
   <Navbar bg="dark" variant="dark" expand="lg">
